0921896ea87e44e5aee5a762b37fa0de05e0b772
joemar25 <joemar.cardino000@gmail.com>
2024-06-11 11:20:15 +0800

    INIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,40 @@
 import { Component } from '@angular/core';
-import { ThemeService } from './core/services/theme.service';
-import { RouterOutlet } from '@angular/router';
-import { NgClass } from '@angular/common';
-import { ResponsiveHelperComponent } from './shared/components/responsive-helper/responsive-helper.component';
+import { CommonService } from './shared/service/common/common.service';
+import { url } from './models/model';
+import { NavigationEnd, Router, Event as RouterEvent } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone: true,
-  imports: [NgClass, RouterOutlet, ResponsiveHelperComponent],
 })
 export class AppComponent {
-  title = 'Angular Tailwind';
+  title = 'dreams-lms';
+  public base = '';
+  public page = '';
+  public last = '';
 
-  constructor(public themeService: ThemeService) {}
+  constructor(private common: CommonService, private Router: Router) {
+    this.common.base.subscribe((base: string) => {
+      this.base = base;
+    });
+    this.common.page.subscribe((page: string) => {
+      this.page = page;
+    });
+    this.common.last.subscribe((last: string) => {
+      this.last = last;
+    });
+    this.Router.events.subscribe((data: RouterEvent) => {
+      if (data instanceof NavigationEnd) {
+        this.getRoutes(Router);
+      }
+    });
+    this.getRoutes(this.Router);
+  }
+  private getRoutes(data: url): void {
+    const splitVal = data.url.split('/');
+    this.common.base.next(splitVal[1]);
+    this.common.page.next(splitVal[2]);
+    this.common.last.next(splitVal[3]);
+  }
 }
